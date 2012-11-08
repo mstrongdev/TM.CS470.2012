59ad3f95ddacae2607ddb066399b64ab1d256f03
Morgan Strong <BMWStrongMA@gmail.com>
2012-11-08 15:49:01 -0700

    Cleaned out our old agent.  Began adding comments for determining algorithm before coding.

--- p2/discovery_agent.py
+++ p2/discovery_agent.py
@@ -22,177 +22,8 @@ def midpoint(pt1, pt2):
 def add(vector1, vector2):
     return (vector1[0] + vector2[0], vector1[1] + vector2[1])
     
-def average(a, b, c, d):
-    return (a + b + c + d) / 4
-
-class PField(object):
-    '''A Potential Field. Parent Class.'''
-    # PUBLIC
-    def __init__(self, center=(0,0), radius=0, spread=100, strength=1, min_bound=None, max_bound=None, direction=None):
-        self.center = center
-        self.radius = radius
-        self.spread = spread
-        self.strength = strength
-        self.min_bound = min_bound
-        self.max_bound = max_bound
-        self.direction = direction
-        
-    def update(self, center=None, radius=None, spread=None, strength=None, min_bound=None, max_bound=None, direction=None):
-        if center:
-            self.center = center
-        if radius:
-            self.radius = radius
-        if spread:
-            self.spread = spread
-        if strength:
-            self.strength = strength
-        if min_bound:
-            self.min_bound = min_bound
-        if max_bound:
-            self.max_bound = max_bound
-        if direction:
-            self.direction = direction
-            
-    def get_force(self, position):
-        return (0,0)
-    
-    # PRIVATE
-    def _attract(self, p, strength=None):
-        d = dist(self.center, p)
-        theta = math.atan2(self.center[1]-p[1], self.center[0]-p[0])
-        scale = strength if strength else self.strength
-        dx,dy = (0,0)
-        
-        # Position is inside the Goal.
-        if d < self.radius: 
-            pass
-        # Position is outside goal and inside spread.
-        elif self.radius <= d and d <= (self.radius+self.spread):
-            dx = scale * (d-self.radius) * math.cos(theta)
-            dy = scale * (d-self.radius) * math.sin(theta)
-        # Position is outside spread. Delta is at max strength.
-        else:
-            dx = scale * self.spread * math.cos(theta)
-            dy = scale * self.spread * math.sin(theta)
-        
-        return (dx,dy)
-        
-    def _gravity(self, p, strength=None):
-        d = dist(self.center, p)
-        theta = math.atan2(self.center[1]-p[1], self.center[0]-p[0])
-        scale = strength if strength else self.strength
-        dx,dy = (0, 0)
-        
-        # doesn't use radius or spread - just strength as a scale on the distance squared
-        if (d > 1):
-            dx = min(scale * (1 / d), 150) * math.cos(theta)
-            dy = min(scale * (1 / d), 150) * math.sin(theta)
-        else:
-            # If right on top of it, don't give a super huge movement force.
-            pass
-        
-        return (dx, dy)
-
-    def _repulse(self, p, strength=None, theta_offset=0):
-        d = dist(self.center, p)
-        theta = math.atan2(self.center[1]-p[1], self.center[0]-p[0]) + theta_offset
-        scale = strength if strength else self.strength
-        dx,dy = (0,0)
-        
-        # Position is inside the Goal.
-        if d < self.radius:
-            dx = -math.cos(theta) * 5 #float("inf")
-            dy = -math.sin(theta) * 5 #float("inf")
-        # Position is outside goal and inside spread.
-        elif self.radius <= d and d <= (self.radius+self.spread):
-            dx = -scale * (self.spread + self.radius - d) * math.cos(theta)
-            dy = -scale * (self.spread + self.radius - d) * math.sin(theta)
-        # Position is outside spread.
-        else:
-            pass
-        
-        return (dx,dy)
-
-    def _tangent(self, p, clockwise, strength=None):
-        theta_direction = math.pi/2 if clockwise else -math.pi/2  # TODO: Does this need to be flipped?
-        return self._repulse(p, strength, theta_direction)
-    
-    def _random(self, p, strength=None, time=0):
-        scale = strength if strength else self.strength
-        
-        #seed = p[0]*41648 + p[1]*915
-        theta = random.random()*2*math.pi - math.pi
-        #seed = p[0]*743 + p[1]*13743
-        magnitude = random.random() * scale
-        
-        dx = magnitude * math.cos(theta)
-        dy = magnitude * math.sin(theta)
-        
-        return (dx,dy)
-
-    def _boundedUniform(self, p):
-        pass
-    def _boundedPerpendicular(self, p):
-        pass
-
-class ObstacleField(PField):
-    
-    def __init__(self, obstacle):
-        new_center = (average(obstacle[0][0], obstacle[1][0], obstacle[2][0], obstacle[3][0]), average(obstacle[0][1], obstacle[1][1], obstacle[2][1], obstacle[3][1]))
-        new_radius = min(dist(new_center, obstacle[0]), dist(new_center, obstacle[1]), dist(new_center, obstacle[2]), dist(new_center, obstacle[3]),
-            dist(new_center, midpoint(obstacle[0], obstacle[1])), dist(new_center, midpoint(obstacle[1], obstacle[2])),
-            dist(new_center, midpoint(obstacle[2], obstacle[3])), dist(new_center, midpoint(obstacle[3], obstacle[0])))
-        new_spread = max(dist(new_center, obstacle[0]), dist(new_center, obstacle[1]), dist(new_center, obstacle[2]), dist(new_center, obstacle[3])) - new_radius + 50
-        self.update(
-            center = new_center,
-            radius = new_radius,
-            spread = new_spread,
-            strength = 1.5)
-    
-    def get_force(self, position):
-        return add(self._repulse(position, 5), self._tangent(position, True))
-
-class FlagField(PField):
-    
-    def __init__(self, flag, agent, num_flags):
-        self.num_flags = num_flags
-        self.update(
-            center = (flag.x, flag.y),
-            radius = float(agent.constants['flagradius']),
-            spread = 50,
-            strength = 1)
-            
-    def get_force(self, position):
-        return add(self._gravity(position, 1500 * self.num_flags), self._attract(position))
-        
-class BaseField(PField):
-    
-    def __init__(self, base):
-        new_center = (average(base.corner1_x, base.corner2_x, base.corner3_x, base.corner4_x), average(base.corner1_y, base.corner2_y, base.corner3_y, base.corner4_y))
-        corner1 = (base.corner1_x, base.corner1_y)
-        corner2 = (base.corner2_x, base.corner2_y)
-        corner3 = (base.corner3_x, base.corner3_y)
-        corner4 = (base.corner4_x, base.corner4_y)
-        new_radius = min(dist(new_center, corner1), dist(new_center, corner2), dist(new_center, corner3), dist(new_center, corner4),
-            dist(new_center, midpoint(corner1, corner2)), dist(new_center, midpoint(corner2, corner3)),
-            dist(new_center, midpoint(corner3, corner4)), dist(new_center, midpoint(corner4, corner1)))
-        new_spread = max(dist(new_center, corner1), dist(new_center, corner2), dist(new_center, corner3), dist(new_center, corner4)) - new_radius
-        self.update(
-            center = new_center,
-            radius = new_radius,
-            spread = new_spread,
-            strength = 100)
-            
-    def get_force(self, position):
-        return self._attract(position)
-        
-class RandomField(PField):
-
-    def __init__(self):
-        self.update(strength = .1)
-        
-    def get_force(self, position):
-        return self._random(position)
+def average(l):
+    return reduce(lambda x, y: x + y, l) / len(l)
 
 class Agent(object):
 
@@ -201,146 +32,27 @@ class Agent(object):
         self.constants = self.bzrc.get_constants()
         self.commands = []
         self.tanks = {tank.index:Tank(bzrc, tank) for tank in self.bzrc.get_mytanks()}
-        
-        ''' Write the initial potential fields ''
-        fields = []
-        for obstacle in self.bzrc.get_obstacles():
-            fields.append(ObstacleField(obstacle))
-            pass
-        for flag in self.bzrc.get_flags():
-            if (flag.color != self.constants['team']):
-                fields.append(FlagField(flag, self, len(self.bzrc.get_flags()) - 1))
-                pass
-        for base in self.bzrc.get_bases():
-            if (base.color == self.constants['team']):
-                #fields.append(BaseField(base))
-                pass
-        fields.append(RandomField());
-        
-        self.write_fields("start.gpi", fields)
-        '''
-        #'''
-        
-    def write_fields(self, file, fields):
-        
-        with open(file, 'w+') as out:
-            # header
-            out.write("set title \"Fields\"\nset xrange [-400.0 : 400.0]\nset yrange [-400.0 : 400.0]\nunset key\nset size square\nset terminal wxt size 1600,1600\nset term png\nset output \"{0}.png\"\n\n".format(file))
-            
-            # write the body of the fields
-            for x in range(-380, 390, 40):
-                for y in range(-380, 390, 40):
-                    vector = (0, 0)
-                    for field in fields:
-                        vector = add(vector, field.get_force((x, y)))
-                    if (vector[0] != float("inf") and vector[1] != float("-inf")):
-                        scaling_factor = 1.0/4.0
-                        out.write("set arrow from {0}, {1} to {2}, {3}\n".format(x - vector[0]/(2 / scaling_factor), y - vector[1]/(2 / scaling_factor), x + vector[0]/(2 / scaling_factor), y + vector[1]/(2 / scaling_factor)))
-                        #pass
-                    
-            # start plot
-            out.write("plot '-' with lines\n0 0 1 1\n")
-                
-            # end plot
-            out.write("e\n");
-        
+        self.bel_grid = [[]]
 
     def tick(self, time_diff):
         '''Some time has passed; decide what to do next'''
-        # Get information from the BZRC server
-        mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
-        self.mytanks = mytanks
-        self.othertanks = othertanks
-        self.flags = flags
-        self.shots = shots
-        self.enemies = [tank for tank in othertanks if tank.color !=
-                self.constants['team']]
-
+        
+        # ALGORITHM
+        # 
+        
+        
         # Reset my set of commands (we don't want to run old commands)
         self.commands = []
-
-        # As obstacles can apparently change with visibility, load them on each tick
-        fields = []
-        #for obstacle in self.bzrc.get_obstacles():
-        #    fields.append(ObstacleField(obstacle))
-        for flag in self.flags:
-            if (flag.color != self.constants['team']):
-                fields.append(FlagField(flag, self, len(self.flags) - 1))
-        #fields.append(RandomField())
         
         # Decide what to do with each of my tanks
         for bot in mytanks:
             tank = self.tanks[bot.index]
             tank.update(bot)
-            # Figure out the desired potential field magic to give it a vector. Make one up for now
-            delta_x = 5
-            delta_y = 10
-            movement = (delta_x, delta_y)
-            
-            tank_pos = (tank.x, tank.y)
-            for field in fields:
-                movement = add(movement, field.get_force(tank_pos))
-            # Return to base?
-            if tank.flag != '-':
-                for base in self.bzrc.get_bases():
-                    if base.color == self.constants['team']:
-                        #print "Returning to base:"
-                        #print tank.flag
-                        movement = add(movement, BaseField(base).get_force(tank_pos))
-            else:
-                ''' This code only attracts to one flag at a time '''
-                # Determine which flag is the one thats being attracted to
-                flag_dist = float("inf")
-                flag_field = None
-                for flag in self.flags:
-                    if (flag.color != self.constants['team']):
-                        #fields.append(FlagField(flag, self, len(self.flags) - 1))
-                        if (dist(tank_pos, (flag.x, flag.y)) < flag_dist):
-                            flag_dist = dist(tank_pos, (flag.x, flag.y))
-                            flag_field = FlagField(flag, self, len(self.flags) - 1)
-                movement = add(movement, flag_field.get_force(tank_pos))
-                '''
-                for flag in self.flags:
-                    if (flag.color != self.constants['team']):
-                        movement = add(movement, FlagField(flag, self, len(self.flags) - 1).get_force(tank_pos))
-                '''
-                #'''
-                # ^ Can I just say how happy I am about this python syntax I came up with all by myself for toggling between two alternate blocks of code?
-            # Combine all fields for filing
-            #tank_fields = list(fields)
-            #tank_fields.append(flag_field)
-            #self.write_fields("fields_{0}.gpi".format(tank.index), tank_fields)
             
             self.commands.append(tank.get_desired_movement_command(movement, time_diff, tank.flag != '-'))
 
         # Send the commands to the server
         results = self.bzrc.do_commands(self.commands)
-
-    '''
-    def attack_enemies(self, bot):
-        #Find the closest enemy and chase it, shooting as you go
-        best_enemy = None
-        best_dist = 2 * float(self.constants['worldsize'])
-        for enemy in self.enemies:
-            if enemy.status != 'alive':
-                continue
-            dist = math.sqrt((enemy.x - bot.x)**2 + (enemy.y - bot.y)**2)
-            if dist < best_dist:
-                best_dist = dist
-                best_enemy = enemy
-        if best_enemy is None:
-            command = Command(bot.index, 0, 0, False)
-            self.commands.append(command)
-        else:
-            self.move_to_position(bot, best_enemy.x, best_enemy.y)
-
-    def move_to_position(self, bot, target_x, target_y):
-        target_angle = math.atan2(target_y - bot.y,
-                target_x - bot.x)
-        relative_angle = normalize_angle(target_angle - bot.angle)
-        command = Command(bot.index, 1, 2 * relative_angle, True)
-        self.commands.append(command)
-    '''
     
 class Tank(object):
     
@@ -389,7 +101,6 @@ class Tank(object):
         # Ignore the PD Controller for now - make sure fields are working first
         return Command(self.index, send_speed, send_angvel, 1 if self.shots_avail and (random.random() * 100 < 3 or has_flag) else 0) # Shoot sporadically
 
-
 def main():
     # Process CLI arguments.
     try:
@@ -418,7 +129,6 @@ def main():
     except KeyboardInterrupt:
         print "Exiting due to keyboard interrupt."
         bzrc.close()
-
 
 if __name__ == '__main__':
     main()
