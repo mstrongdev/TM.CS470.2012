aace7006319ce5b66cb952c6098aa6f7449ab736
Morgan Strong <BMWStrongMA@gmail.com>
2012-12-01 13:57:57 -0700

    removed old p2 code from kalman_agent.py

--- p3/kalman_agent.py
+++ p3/kalman_agent.py
@@ -161,79 +161,34 @@ class Agent(object):
             out.write("e\n");
             '''
     
-    def update_belief(self, row, col, grid_val):
-        '''Update the belief grid based on Bayes Rule'''
-        if grid_val == 1:  # if we observe a hit
-            Bel_Occ = float(self.constants["truepositive"]) * self.bel_grid[row,col]
-            Bel_Unocc = (1-float(self.constants["truenegative"])) * (1-self.bel_grid[row,col])
-            # Normalize
-            self.bel_grid[row,col] = Bel_Occ / (Bel_Occ + Bel_Unocc);
-            
-        else:  # If do not observe a hit
-            Bel_Occ = (1-float(self.constants["truepositive"])) * self.bel_grid[row,col]
-            Bel_Unocc = float(self.constants["truenegative"]) * (1-self.bel_grid[row,col])
-            # Normalize
-            self.bel_grid[row,col] = Bel_Occ / (Bel_Occ + Bel_Unocc)
-    
-    
     def tick(self, time_diff):
         '''Some time has passed; decide what to do next'''
-        
-        # ALGORITHM
-        # 
-        # for all_tanks
-        #  if should_sample()
-        #   call occgrid
-        #   update bel/conf grid
-        #  if picknewpoint()
-        #   picknewpoint()
-        #  movetopoint()
-        # 
         
         # Reset my set of commands (we don't want to run old commands)
         self.commands = []
         
-        # Decide what to do with each of my tanks
-        #width, height = self.conf_grid.shape
-        #average_confidence = numpy.average(self.conf_grid)
         for bot in self.bzrc.get_mytanks():
+            # ALGORITHM
+            # 
+            # for all_tanks
+            #  if should_sample()
+            #   call occgrid
+            #   update bel/conf grid
+            #  if picknewpoint()
+            #   picknewpoint()
+            #  movetopoint()
+            # 
+            
             # Get the tank and update it with what we received from the server
             tank = self.tanks[bot.index]
             tank.update(bot)
             
-            '''
-            # Check to see if the tank should take a sample now
-            if tank.should_sample(self.conf_grid):
-                # Call occgrid
-                pos, grid = self.bzrc.get_occgrid(bot.index)
-                
-                # Iterate over each cell in the sampled grid
-                for col in range(pos[0] + 400, pos[0] + 400 + grid.shape[0]):
-                    for row in range(pos[1] + 400, pos[1] + 400 + grid.shape[1]):
-                        #print col, row
-                        # Update belief grid
-                        self.update_belief(row, col, grid[col-pos[0]-400,row-pos[1]-400])
-                        # Update confidence grid
-                        self.conf_grid[row, col] += .1
-                
-                # This code is replaced with the loop above
-                # Update confidence grid
-                # With the rounding here, this might have some off by one errors...when adding the updates for the belief grid, this should be changed as well, so that only the cells changed are updated.
-                #for col in range(int(max(0, tank.x + 350)), int(min(width, tank.x + 450))): # TODO: AGAIN: THESE ASSUME A CENTERED, 800x800 WORLD
-                #    for row in range(int(max(0, tank.y + 350)), int(min(height, tank.y + 450))):
-                #        #self.conf_grid[row, col] += .1
-                #        pass
-                #self.conf_grid = numpy.add(self.conf_grid, 1)
-                
-            tank.check_pick_new_point(average_confidence, time_diff)
-            '''
             
             self.commands.append(tank.get_desired_movement_command(time_diff, int(self.constants["tankspeed"])))
 
         # Send the movement commands to the server
         results = self.bzrc.do_commands(self.commands)
         
-        #update_grid_display(self.bel_grid)
     
 class Tank(object):
     
@@ -266,61 +221,6 @@ class Tank(object):
         self.vx = tank.vx;
         self.vy = tank.vy;
         self.angvel = tank.angvel;
-        
-    def check_pick_new_point(self, average_confidence, time_diff):
-        delta_x = self.target[0] - self.x
-        delta_y = self.target[1] - self.y
-        #error_angle = normalize_angle(math.atan2(delta_y, delta_x) - normalize_angle(self.angle));
-        #print "speed:",dist((self.vx, self.vy), (0, 0))/time_diff
-        #if dist((self.x, self.y), self.target) < 20 or (dist((self.vx, self.vy), (0, 0))/time_diff < 1 and math.fabs(error_angle) < math.pi/6):
-        
-        magnitude = math.sqrt(delta_x**2 + delta_y**2)
-        direction = (delta_x / magnitude, delta_y / magnitude)
-        
-        if dist((self.x, self.y), self.target) < 20 or (average_grid(self.agent.bel_grid, (self.x + 5 * direction[0] + 400, self.y + 5 * direction[1] + 400), 10) > .8) or (self.x == self.prev_x and self.y == self.prev_y):
-            # We need a new point
-            self.pick_point(average_confidence)
-        else:
-            #print "no new target"
-            pass
-        
-    def pick_point(self, average_confidence):
-        #newx = random.randint(-400, 400)
-        #newy = random.randint(-400, 400)
-        #attempts = 0
-        #while average_grid(self.agent.conf_grid, (newx + 400, newy + 400), 100) > average_confidence and attempts < 5:
-        #    attempts += 1
-        #    newx = random.randint(-400, 400)
-        #    newy = random.randint(-400, 400)
-        #self.target = (newx, newy)
-        self.target = self.pick_recursive(self.agent.conf_grid, 0, 800, 0, 800, 10)
-        print "New target:", self.target
-        
-    def pick_recursive(self, grid, left, right, bottom, top, depth):
-        if depth == 0:
-            return (random.randint(left, right)-400, random.randint(bottom, top)-400)
-
-        width = right - left
-        height = top - bottom
-        left_split = numpy.average(grid[bottom:top, left + width/2:right])
-        right_split = numpy.average(grid[bottom:top, left:right - width/2])
-        bottom_split = numpy.average(grid[bottom + height/2:top, left:right])
-        top_split = numpy.average(grid[bottom:top - height/2, left:right])
-        min_avg = min(left_split, right_split, bottom_split, top_split)
-        if left_split == min_avg:
-            return self.pick_recursive(grid, left + width/2, right, bottom, top, depth - 1)
-        elif right_split == min_avg:
-            return self.pick_recursive(grid, left, right - width/2, bottom, top, depth - 1)
-        elif bottom_split == min_avg:
-            return self.pick_recursive(grid, left, right, bottom + height/2, top, depth - 1)
-        else:
-            return self.pick_recursive(grid, left, right, bottom, top - height/2, depth - 1)
-        
-    def should_sample(self, conf_grid):
-        # TODO: THE 400s HERE ARE TO MOVE COORDINATES INTO ALL POSITIVE SPACE. THE REGULAR WORLD IS CENTERED AROUND THE POINT (0, 0)
-        # this 10 is our hard coded decision of when we feel like we won't get any more benefit from sampling this region
-        confidence_threshold = .1
-        return average_grid(conf_grid, (self.x + 400, self.y + 400), 100) < confidence_threshold
     
     def get_desired_movement_command(self, time_diff, maxspeed):
         # PD Controller stuff to make movement smoother
@@ -369,40 +269,6 @@ class Tank(object):
             
         return Command(self.index, send_speed, send_angvel, 1)
 
-# OpenGL functions
-window = None
-grid = None
-
-def draw_grid():
-    # This assumes you are using a numpy array for your grid
-    width, height = grid.shape
-    glRasterPos2f(-1, -1)
-    glDrawPixels(width, height, GL_LUMINANCE, GL_FLOAT, grid)
-    glFlush()
-    glutSwapBuffers()
-
-def update_grid_display(new_grid):
-    global grid
-    grid = new_grid
-    draw_grid()
-
-def init_window(width, height):
-    global window
-    global grid
-    grid = numpy.zeros((width, height))
-    glutInit(())
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-    glutInitWindowSize(width, height)
-    glutInitWindowPosition(0, 0)
-    #window = glutCreateWindow("Grid filter lab: Confidence")
-    window = glutCreateWindow("Grid filter lab: Belief")
-    glutDisplayFunc(draw_grid)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    #glutMainLoop()
-        
 def main():
     # Process CLI arguments.
     try:
